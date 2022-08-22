d8b105d724ebf3f5545ad06a18df2857baac56c9
cyll99 <christyanlove@gmail.com>
2022-08-22 14:00:38 -0400

    third video

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -2,14 +2,26 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
 public class ComposeActivity extends AppCompatActivity {
+    TwitterClient client;
     public static final int MAX_LINES = 140;
+    public static final String TAG = "ComposeActivity";
     EditText etCompose;
     Button btnTweet;
 
@@ -17,6 +29,8 @@ public class ComposeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        client = TwitterApp.getRestClient(this);
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
@@ -35,6 +49,28 @@ public class ComposeActivity extends AppCompatActivity {
                 return;
             }
             Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+            client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Headers headers, JSON json) {
+                    Log.i(TAG,"on success load more data");
+                    try {
+                        Tweet tweet = Tweet.fromJson(json.jsonObject);
+                        Log.i(TAG,"published tweet is : " + tweet.body);
+                        Intent intent = new Intent();
+                        intent.putExtra("tweet", Parcels.wrap(tweet));
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                    Log.e(TAG,"on failure to publish tweet",  throwable);
+
+                }
+            });
 
         }
     });
